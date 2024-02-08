e4933dc270dc754bc961c381a2b57b983cbdb6cd
HannahWhitham <whitham.hannah@gmail.com>
2024-02-08 18:35:43 +0000

    A basic leaflet map is loaded in the InteractiveMap page

--- fixed-frontend/src/app/components/map-components/InteractiveMap.js
+++ fixed-frontend/src/app/components/map-components/InteractiveMap.js
@@ -1,25 +1,25 @@
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer } from 'react-leaflet';
+import "leaflet-defaulticon-compatibility"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 const centralLocation = [51.505, -0.09];//default for now
 const zoom = 12;
 
 const InteractiveMap = () => {
     return ( 
-        <div className="interactive-map">
-            <MapContainer center={centralLocation} zoom = {zoom} scrollWheelZoom={false}>
+            <MapContainer className="interactive-map" style={{height: "100vh", width: "100vw"}} center={centralLocation} zoom = {zoom} scrollWheelZoom={false}>
                 {/* Get data from OSM */}
                 <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
 
                 {/* Add a marker to the center of the map */}
-                {/* <Marker position={centralLocation}>
+                <Marker position={centralLocation}>
                     <Popup>
                         Hello World!
                     </Popup>
-                </Marker> */}
+                </Marker>
 
             </MapContainer>
-        </div>
      );
 }
  
--- fixed-frontend/src/app/dashboard/HomePage.js
+++ fixed-frontend/src/app/dashboard/HomePage.js
@@ -1,5 +1,6 @@
 import EnviromentContainer from "../components/enviroment";
 import LocationCountBox from "../components/locationNumber";
+import InteractiveMap from "../components/map-components";
 
 
 const HomePage = () => {
