0411d8cb277f02fb810c3d28b74661a915ebfef6
Dan <d.swash@lancaster.ac.uk>
2024-02-08 14:03:10 +0000

    Cleanup

--- fixed-frontend/src/app/dashboard/page.js
+++ fixed-frontend/src/app/dashboard/page.js
@@ -27,15 +27,6 @@ export function sendNotification(title, options) {
 }
 // sendNotification('Hello!', { body: 'You have a new message.'});
 
-function Card({ title, children, width, height }) {
-  return (
-    <div className="card" style={{ width: width, height: height }}>
-      <div className="card-title">{title}</div>
-      <div className="card-content">{children}</div>
-    </div>
-  );
-}
-
 function CardContainer({ children }) {
   return <div className="card-container">{children}</div>;
 }
@@ -49,51 +40,27 @@ function UserButton({ username }) {
   );
 }
 
-function FrontPage(){
-  return(
-    // <CardContainer>
-    <>
-        <Card title="Card 1" width="200px" height="200px">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Card>
-        <Card title="Card 2" width="200px" height="200px">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Card>
-        <Card title="Card 3" width="200px" height="200px">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
-        <Card title="Card 4" width="400px" height="400px">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Card>
-        <Card title="Card 5" width="300px" height="200px">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card>
-        <Card title="Card 6" width="300px" height="200px">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Card>
-      {/* </CardContainer> */}
-      </>
-  );
-}
 
 function InteractivePage(){
   return(
     <>
-      
       <EnviromentContainer/>
       <LocationCountBox/>
     </>
   )
 }
 
-// function Table(){
-//   return(
-//     <CardContainer>
-      
-//     </CardContainer>
-//   )
-// }
+
 
 export default function Dashboard() {
   const views = {
-    "frontpage": <FrontPage/>,
+
     "table": <PeopleTable/>,
     "test": <InteractivePage/>
   }
 
   const[currentView,setView] = useState("test")
 
-  // function showView(viewName){
-  //   this.setState({displayedView: viewName})
-  // }
 
   return (
     <body>
@@ -106,14 +73,6 @@ export default function Dashboard() {
         <h1 className="title">Dashboard</h1>
         <UserButton username="Username" />
       </div>
-      {/* <CardContainer>
-        <Card title="Card 1" width="200px" height="200px">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Card>
-        <Card title="Card 2" width="200px" height="200px">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Card>
-        <Card title="Card 3" width="200px" height="200px">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
-        <Card title="Card 4" width="400px" height="400px">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Card>
-        <Card title="Card 5" width="300px" height="200px">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card>
-        <Card title="Card 6" width="300px" height="200px">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Card>
-      </CardContainer> */}
       <CardContainer>
        {views[currentView]}
       </CardContainer>
