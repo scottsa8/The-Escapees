bf2ceb15d4f1f89a5019444a9c5c988897d76883
Dan <d.swash@lancaster.ac.uk>
2024-02-08 14:21:13 +0000

    move notifications

---
+++ fixed-frontend/src/app/components/notifications.js
@@ -0,0 +1,21 @@
+export function sendNotification(title, options) {
+    // Check browser support
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    // Check for permission
+    else if (Notification.permission === "granted") {
+      new Notification(title, options);
+    }
+  
+    // Request permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {
+          new Notification(title, options);
+        }
+      });
+    }
+  
+  }
