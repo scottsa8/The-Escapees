6d6b772d74430f44c7441a737a3e78738167a553
Adam Strawbridge <a.strawbridge@lancaster.ac.uk>
2024-02-15 18:45:44 +0000

    Added a network settings popup to the log in page

--- fixed-frontend/src/app/page.js
+++ fixed-frontend/src/app/page.js
@@ -4,20 +4,30 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useId } from 'react';
 import Image from "next/image";
-import {settingsOutline} from 'ionicons/icons'
 import "./main.css"
 import { network } from './layout';
 import {setCookie} from './components/cookies'
-import { IonButton, IonIcon } from '@ionic/react';
+import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+
 
 
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const router = useRouter();
 
+  const [ip, setIP] = useState(network.ip);
+  const [port, setPort] = useState(network.port);
+
+  const [isSettingsOpen,setSettingsOpen] = useState(false)
+  const router = useRouter();
   const usernameId = useId();
   const passwordId = useId();
+  const ipId = useId();
+  const portId = useId();
+
+  const toggleSettings = () => {
+    setSettingsOpen(!isSettingsOpen)
+  }
 
   // const login = async (username, password) => {
   //   console.log("Validating")
@@ -69,6 +79,13 @@ export default function Login() {
     }
   };
 
+  const handleNetworkSave = () => {
+    console.log("IP: %s", ip )
+    console.log("Port: %s", port)
+    network.ip = ip;
+    network.port = port;
+  }
+
   // function handleModal(ip,port){
   //   console.log(`ip: ${ip}, port: ${port}`)
   // }
@@ -83,7 +100,7 @@ export default function Login() {
     // </div>
 
     
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center flex-col justify-center h-screen">
       <div className=" flex flex-col w-5/6 h-64 sm:mx-auto sm:max-w-md bg-slate-300 rounded p-2">
 
           <h2 className="pl-2 text-slate-700">
@@ -106,14 +123,33 @@ export default function Login() {
               
               <section className="flex flex-row-reverse justify-between">
                   <button type="submit" className="bg-slate-100 rounded p-1 border w-24 h-12 border-slate-900 ">Submit</button>
-                  <IonButton>
-                    <IonIcon slot='icon-only'className='w-10 h-10' icon={settingsOutline} ></IonIcon>
-                  </IonButton>
+                      
               </section>
-
-
           </form> 
+          <button className="bg-slate-100 rounded p-1 border w-12 h-12 border-slate-900 items-center" 
+                  onClick={toggleSettings}
+                  aria-label='Network settings'>
+                    <Cog6ToothIcon className=" w-10 h-10 " alt="Settings cog"/>
+          </button>
       </div>
+      {isSettingsOpen && (
+            <div className='flex flex-col w-5/6 min-h-48 sm:mx-auto sm:max-w-md bg-slate-300 rounded p-2 border border-slate-800 ' onSubmit={handleNetworkSave}>
+              <div id="input-fields" className="justify-between flex flex-col grow">    
+                  <label htmlFor={ipId} >
+                      IP Address: <input name="IP address" type="text"  
+                      value={ip} onChange={e => setIP(e.target.value)} />
+                  </label>
+                  
+                  <label htmlFor={portId} className="">
+                      Port: <input name="Port" type="text" 
+                      value={port} onChange={e => setPort(e.target.value)} />
+                  </label>
+              </div>
+              <section className="flex flex-row-reverse justify-between">
+                <button onClick={handleNetworkSave}  className="bg-slate-100 rounded p-1 border w-24 h-12 border-slate-900 ">Save</button>
+              </section>
+            </div>
+          )}
     </div>
   );
 }
