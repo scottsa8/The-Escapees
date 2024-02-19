7651d2befd0fc095b6c3fbaba63ba417ef40805b
Daniel <d.swash@lancaster.ac.uk>
2024-02-19 13:27:26 +0000

    Bug fix for location number

--- fixed-frontend/src/app/components/locationNumber.js
+++ fixed-frontend/src/app/components/locationNumber.js
@@ -2,68 +2,30 @@ import { useEffect, useState } from "react";
 import { network } from "../layout";
 
 
-function RoomCard({ roomName, prisonerCount, guardCount, onClick, isSelected}) {
+function RoomCard({ roomName, onClick, isSelected}) {
+    const [inmateCount,setInmateCount] = useState(0)
+    const [guardCount,setGuardCount] = useState(0)
 
+    useEffect(() => {
+      fetch(`http://${network.ip}:${network.port}/getPeople?loc=${roomName}$type=inmate`)
+        .then(response => response.json())
+        .then(num => setInmateCount(num));
+      fetch(`http://${network.ip}:${network.port}/getPeople?loc=${roomName}$type=guard`)
+        .then(response => response.json())
+        .then(num => setGuardCount(num));
+      }, []);
 
     const cardStyle = isSelected ? { backgroundColor: '#1B2030', cursor: 'pointer' } : { cursor: 'pointer' };
 
     return (
       <div onClick={onClick} style={cardStyle} className="bg-white shadow-md rounded-lg p-4 m-4 max-w-sm w-60 dark:bg-gray-700 dark:text-blue-100">
         <h2 className="text-xl font-semibold text-gray-800 dark:text-blue-300">{`${roomName}`}</h2>
-        <p className="text-gray-600 mt-2 dark:text-blue-100">{`Prisoners: ${prisonerCount}`}</p>
+        <p className="text-gray-600 mt-2 dark:text-blue-100">{`Prisoners: ${inmateCount}`}</p>
         <p className="text-gray-600 mt-2 dark:text-blue-100">{`Guards: ${guardCount}`}</p>
       </div>
     );
 } 
 
-
-function LocationCount({location}){
-  const [inmateCount,setInmateCount] = useState(0)
-  const [guardCount,setGuardCount] = useState(0)
-
-
-    useEffect(() => {
-      fetch(`http://${network.ip}:${network.port}/getPeople?loc=${location}$type=inmate`)
-        .then(response => response.json())
-        .then(num => setInmateCount(num));
-      fetch(`http://${network.ip}:${network.port}/getPeople?loc=${location}$type=guard`)
-        .then(response => response.json())
-        .then(num => setGuardCount(num));
-      console.log(inmateCount, guardCount);
-      }, []);
-
-    // const getPeople = async () => {
-    //     const response = await fetch(`http://${network.ip}:${network.port}/getPeople?loc=${location}`)
-    //     const data = await response.json();
-        
-    //     data.then((data) => setCount(data));
-    // };
-
-    // useEgetPeople()
-    // useEffect(()=>{
-    //     const getPeople = async () => {
-    //         const response = await fetch(`http://${network.ip}:${network.port}/getPeople?loc=${location}`)
-    //         const data = await response.json();
-            
-    //         data.then((data) => setCount(data));
-    //     };
-    //     getPeople()
-    //     const interval=setInterval(()=>{
-    //         getPeople()
-    //         },10000)
-       
-       
-    //       return()=>clearInterval(interval)
-    // },[])
-
-    return(
-        <div className="flex flex-col w-12 min-h-49 border rounded border-neutral-900 m-0.5 grow">
-            <label className="bg-neutral-600 text-center text-xl text-neutral-200">{location}</label>
-            <label className="text-center text-9xl bg-orange-200">{count}</label>
-        </div>
-    )
-
-}
 
 export default function LocationCountBox({onRoomClick}){
 
@@ -79,38 +41,12 @@ export default function LocationCountBox({onRoomClick}){
         });
     }, []);
 
-    
-
-    // useEffect(() => {
-    // const getLocations = async () => {
-    //     const response = await fetch(`http://${network.ip}:${network.port}/getAllNames`)
-    //     const data = await response.json();
-    //     data.then((data) => setLocation(data));
-    // };
-    //     getLocations()
-
-    // },[])
-
     return(
-        
-        // <div id="container" className="w-full flex flex-col border border-neutral-900 rounded m-0.5">
-        //     <div className="bg-neutral-300 flex justify-center"> 
-        //     <label className=" text-center text-xl text-neutral-900">Location Count</label>
-        //     </div>
-            
-        //     <div id="content" className="flex">
-        //         {locations.map( (location) => (
-        //             <LocationCount
-        //             location={location.name}/>
-        //         ) )}
-        //     </div>
-        // </div>
-
-    <div className="w-full min-w-0 flex flex-wrap justify-start space-between">
-        {locations.map(room => (
-        <RoomCard roomName={room} prisonerCount={inmateCount} guardCount={guardCount} onClick={() => {setSelectedRoom(room); onRoomClick(room);}} isSelected={selectedRoom === room}/>
-        ))}
-    </div>
+      <div className="w-full min-w-0 flex flex-wrap justify-start space-between">
+          {locations.map(room => (
+          <RoomCard roomName={room} onClick={() => {setSelectedRoom(room); onRoomClick(room);}} isSelected={selectedRoom === room}/>
+          ))}
+      </div>
 
     )
 
