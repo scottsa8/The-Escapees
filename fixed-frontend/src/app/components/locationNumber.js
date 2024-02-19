152699ba9ce666b47997b02a69916196f20528a9
Dan <d.swash@lancaster.ac.uk>
2024-02-19 17:40:06 +0000

    Styling fixes in light mode

--- fixed-frontend/src/app/components/locationNumber.js
+++ fixed-frontend/src/app/components/locationNumber.js
@@ -15,14 +15,14 @@ function RoomCard({ roomName, onClick, isSelected}) {
         .then(num => setGuardCount(num));
       }, []);
 
-    const cardStyle = isSelected ? { backgroundColor: '#1B2030', cursor: 'pointer' } : { cursor: 'pointer' };
+    const cardStyle = isSelected ? {color: 'DBE9FE', backgroundColor: '#1B2030', cursor: 'pointer' } : { cursor: 'pointer' };
 
     return (
       <div onClick={onClick} style={cardStyle} className="bg-white shadow-md rounded-lg p-4 m-4 max-w-sm w-60 dark:bg-gray-700 dark:text-blue-100">
-        <h2 className="text-xl font-semibold text-gray-800 dark:text-blue-300">{`${roomName}`}</h2>
-        <p className="text-gray-600 mt-2 dark:text-blue-100">{`Prisoners: ${inmateCount}`}</p>
-        <p className="text-gray-600 mt-2 dark:text-blue-100">{`Guards: ${guardCount}`}</p>
-        <p className="text-gray-600 mt-2 dark:text-blue-100">{`Total: ${guardCount+inmateCount}`}</p>
+        <h2 className="text-xl font-semibold text-gray-600 dark:text-blue-300">{`${roomName}`}</h2>
+        <p className="text-gray-500 mt-2 dark:text-blue-100">{`Prisoners: ${inmateCount}`}</p>
+        <p className="text-gray-500 mt-2 dark:text-blue-100">{`Guards: ${guardCount}`}</p>
+        <p className="text-gray-500 mt-2 dark:text-blue-100">{`Total: ${guardCount+inmateCount}`}</p>
       </div>
     );
 } 
