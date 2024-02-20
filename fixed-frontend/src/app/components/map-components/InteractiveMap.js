46b15bcad36ad65f6036e824a27578da8cfbbc0f
HannahWhitham <whitham.hannah@gmail.com>
2024-02-20 20:45:29 +0000

    zoom control moved

--- fixed-frontend/src/app/components/map-components/InteractiveMap.js
+++ fixed-frontend/src/app/components/map-components/InteractiveMap.js
@@ -1,7 +1,7 @@
 import 'leaflet/dist/leaflet.css'
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
-import { MapContainer, TileLayer, Marker, Popup, FeatureGroup, Polyline} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, FeatureGroup, Polyline, ZoomControl} from 'react-leaflet';
 import "leaflet-draw/dist/leaflet.draw.css";
 import { EditControl } from 'react-leaflet-draw';
 import { useEffect, useState } from 'react';
@@ -145,11 +145,11 @@ const InteractiveMap = () => {
 
     return ( 
         <div>
-            <MapContainer className="interactive-map" center={lancasterPrisonLongLat} zoom = {zoom} scrollWheelZoom={true}>
-
+            <MapContainer className="interactive-map" zoom={zoom} center={lancasterPrisonLongLat} scrollWheelZoom={true} zoomControl = {false}> 
+                <ZoomControl position = "topright"/>
                 {/* The sidepannel with shape drawing options */}
                 <FeatureGroup>
-                    <EditControl position="topleft" onCreated={onCreate} onEdited={onEdit} onDeleted = {onDelete} draw={{
+                    <EditControl position="topright" onCreated={onCreate} onEdited={onEdit} onDeleted = {onDelete} draw={{
                         // Disabling the other draw features so there is onlt a polygon option
                       rectangle: false,
                       circle: false,
