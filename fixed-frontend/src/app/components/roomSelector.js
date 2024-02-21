4513871539a90995ca8a95dd4afd6f49e618c335
Dan <d.swash@lancaster.ac.uk>
2024-02-21 10:28:46 +0000

    RoomSelector Component

---
+++ fixed-frontend/src/app/components/roomSelector.js
@@ -0,0 +1,36 @@
+import {useEffect, useState} from 'react';
+import {network} from '../layout';
+
+export default function RoomSelector({onLocationChange}){
+  const [locations, setLocations] = useState([]);
+  const [selectedRoom, setSelectedRoom] = useState("");
+
+
+  useEffect(() => {
+    fetch(`http://${network.ip}:${network.port}/getRooms`,
+    {mode: 'cors',headers: {'Access-Control-Allow-Origin':'*'}})
+      .then(response => response.json())
+      .then(data => {
+        const roomNames = data.rooms.data
+        .map(roomObj => roomObj.room)
+        .filter(roomName => !roomName.includes('gate'));
+        setLocations(roomNames);
+        setSelectedRoom(roomNames[3]);
+      });
+  }, []);
+
+  const handleRoomChange = (e) => {
+    setSelectedRoom(e.target.value);
+    onLocationChange(event.target.value);
+  };
+
+  return (
+  <div className="roomSelector">
+    <select value={selectedRoom} onChange={handleRoomChange} className="p-2 border rounded-md shadow-md">
+      {locations.map((location, index) => (
+        <option key={index} value={location}>{location}</option>
+      ))}
+    </select>
+  </div>
+  );
+}
