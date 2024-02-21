ef6edff1837facf7a55170f29d155cd83ed83697
HannahWhitham <whitham.hannah@gmail.com>
2024-02-21 14:10:20 +0000

    resolved merge errors

--- fixed-frontend/src/app/components/EnvironmentBox.js
+++ fixed-frontend/src/app/components/EnvironmentBox.js
@@ -1,10 +1,10 @@
 import Dial from "./dial";
 import { sendNotification } from "./notifications";
 
-const EnvironmentBox = ({measurement, value, max}) => {
+const EnvironmentBox = ({size, dialClassName, measurement, value, max}) => {
     return ( 
     <div className="flex flex-col items-center p-4 bg-transparent">
-        <Dial size={size} value={value} min={0} max={max} onMaxValue={() => {
+        <Dial className={dialClassName} size={size} value={value} min={0} max={max} onMaxValue={() => {
             sendNotification('Max Value Reached', { body: `Measurement: ${measurement}, Value: ${value}`});
         }}/>
         <span className="text-lg font-medium mt-2">{measurement}</span>
