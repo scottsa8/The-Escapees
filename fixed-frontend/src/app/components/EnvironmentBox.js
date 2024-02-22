a8581904d3dae961406d17f9865bfe0742e123bd
Daniel <d.swash@lancaster.ac.uk>
2024-02-22 09:11:12 +0000

    notif fix

--- fixed-frontend/src/app/components/EnvironmentBox.js
+++ fixed-frontend/src/app/components/EnvironmentBox.js
@@ -1,13 +1,15 @@
 import Dial from "./dial";
-import { sendNotification } from "./notifications";
+import { useNotification } from "./notifications";
 
 const EnvironmentBox = ({size, dialClassName, measurement, value, max}) => {
+    const { sendNotification, NotificationComponent } = useNotification();
     return ( 
     <div className="flex flex-col items-center p-4 bg-transparent">
         <Dial className={dialClassName} size={size} value={value} min={0} max={max} onMaxValue={() => {
-            sendNotification('Max Value Reached', { body: `Measurement: ${measurement}, Value: ${value}`});
+            sendNotification('Max Value Reached', 'Measurement: ${measurement}, Value: ${value}');
         }}/>
         <span className="text-lg font-medium mt-2">{measurement}</span>
+    <NotificationComponent />
     </div> 
     );
 }
