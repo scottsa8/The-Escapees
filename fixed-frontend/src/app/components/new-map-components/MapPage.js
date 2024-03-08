75b1eea65dac44016775f6c3f713a101b15c05b7
HannahWhitham <whitham.hannah@gmail.com>
2024-03-08 16:23:23 +0000

    started working on way to load a map from the database and add a map to database

--- fixed-frontend/src/app/components/new-map-components/MapPage.js
+++ fixed-frontend/src/app/components/new-map-components/MapPage.js
@@ -1,16 +1,26 @@
 import RoomCanvas from "./RoomCanvas";
 import { useState } from "react";
 import {SelectUserBox} from "./SelectUserBox";
+import {UploadMap} from "./UploadMap";
 
 const MapPage = () => {
 
     const [selectedUser, setSelectedUser] = useState(null);
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (e) =>{
+        let selectedFile = e.target.files[0];
+        console.log(selectedFile);
+    }
 
     return ( 
         <div className="MapPage">
             {/* <div className="flex mb-5">
                 <SelectUserBox onLocationChange={setSelectedUser} />
             </div> */}
+            <form>
+                <input type={"file"} onChange={handleFileChange} accept={".csv"}></input>
+            </form>
             <RoomCanvas selectedUser={selectedUser}></RoomCanvas>
         </div>
      );
