0f1a0588f78108c09e87459158ba14b7b2b0a5b6
Dan <d.swash@lancaster.ac.uk>
2024-03-12 15:57:13 +0000

    microbit manager

--- fixed-frontend/src/app/components/new-map-components/MapPage.js
+++ fixed-frontend/src/app/components/new-map-components/MapPage.js
@@ -187,7 +187,7 @@ const MapPage = () => {
     }
 
     return ( 
-        <div className="MapPage">
+        <div className="MapPage card-container">
             <div className="flex mb-5">
                 { <SelectUserBox /> }
             </div>
