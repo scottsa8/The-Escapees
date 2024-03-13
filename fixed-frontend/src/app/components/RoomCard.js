7c5574e9cb2280a1d53d5c70ca5263d4513c8cdd
Daniel <d.swash@lancaster.ac.uk>
2024-03-13 16:18:17 +0000

    bug fixes

--- fixed-frontend/src/app/components/RoomCard.js
+++ fixed-frontend/src/app/components/RoomCard.js
@@ -23,16 +23,31 @@ const RoomCard = ({roomName, onClick, isSelected}) => {
           : "bg-white dark:bg-gray-700 text-gray-600 dark:text-blue-100 shadow-md rounded-lg p-4 m-4 max-w-sm w-60 cursor-pointer";
 
     return (
-      <motion.div onClick={onClick} className={cardStyle}
-        whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.95 }}
-        transition={{ type: "spring", stiffness: 400, damping: 17 }}>
-        <h2 className="text-xl font-semibold text-gray-600 dark:text-blue-300">{`${roomName}`}</h2>
-        {types.map((type, index) => (
-          <p key={index} className="text-gray-500 mt-2 dark:text-blue-100">{`${type.charAt(0).toUpperCase() + type.slice(1)}: ${userCounts[index]}`}</p>
-        ))}
-        <p className="text-gray-500 mt-2 dark:text-blue-100">{`Total: ${Object.values(userCounts).reduce((sum, count) => sum + count, 0)}`}</p>
-      </motion.div>
+      <>
+        {!isLoading ? (
+          <motion.div
+            onClick={onClick}
+            className={cardStyle}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ type: "spring", stiffness: 400, damping: 17 }}
+          >
+            <h2 className="text-xl font-semibold text-gray-600 dark:text-blue-300">{`${roomName}`}</h2>
+            {types.map((type, index) => (
+              <p
+                key={index}
+                className="text-gray-500 mt-2 dark:text-blue-100"
+              >{`${type.charAt(0).toUpperCase() + type.slice(1)}: ${userCounts[index]}`}</p>
+            ))}
+            <p className="text-gray-500 mt-2 dark:text-blue-100">{`Total: ${Object.values(userCounts).reduce(
+              (sum, count) => sum + count,
+              0
+            )}`}</p>
+          </motion.div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </>
     );
 }
  
