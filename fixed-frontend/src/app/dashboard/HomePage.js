bda93aab37f57e94fd3ef7dd2c906a76f6f5249c
scott <s.a.smith5@lancaster.ac.uk>
2024-03-13 15:40:30 +0000

    fixed set cookies

--- fixed-frontend/src/app/dashboard/HomePage.js
+++ fixed-frontend/src/app/dashboard/HomePage.js
@@ -1,8 +1,9 @@
+import { setCookie } from "../components/cookies";
 import EnviromentContainer from "../components/enviroment";
 import LocationCountBox from "../components/locationNumber";
 
 const HomePage = () => {
-
+    setCookie("types","inmates,guards")
     return (
         <>
             <EnviromentContainer/>
